34e4a13113f8531a41235e8e68216df32793edf2
SneyderHOL <1507@holbertonschool.com>
2021-04-23 16:33:29 -0500

    Update reducing duplication using helper methods

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -10,16 +10,19 @@ end
 RSpec.describe Card do
   # before(:example)  is the same as before do
   # symbol could also be :suit, :context
-  before do
-    @card = Card.new('Ace', 'Spades') 
+  # before do
+  #   @card = Card.new('Ace', 'Spades')
+  # end
+  def card
+    Card.new('Ace', 'Spades')
   end
 
   # 'specify' alias for 'it'
   it 'has a rank' do
-    expect(@card.rank).to eq('Ace')
+    expect(card.rank).to eq('Ace')
   end
 
   it 'has a suit' do
-    expect(@card.suit).to eq('Spades')
+    expect(card.suit).to eq('Spades')
   end
 end
