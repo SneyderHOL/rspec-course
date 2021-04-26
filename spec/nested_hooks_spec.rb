8ec7daef9e1318e040db2b850c181846bd99bb65
SneyderHOL <1507@holbertonschool.com>
2021-04-26 09:51:35 -0500

    Using nested logic with hooks

---
+++ spec/nested_hooks_spec.rb
@@ -0,0 +1,39 @@
+RSpec.describe 'nested hooks' do
+  before(:context) do
+    puts 'OUTER Before context'
+  end
+
+  before(:example) do
+    puts 'OUTER Before example'
+  end
+
+  it 'does basic math' do
+    expect(1 + 1).to eq(2)
+  end
+
+  context 'with condition A' do
+    before(:context) do
+      puts 'INNER Before context'
+    end
+  
+    before(:example) do
+      puts 'INNER Before example'
+    end
+
+    it 'does some more basic math' do
+      expect(1 + 1).to eq(2)
+    end
+
+    it 'does substraction as well' do
+      expect(5 - 3).to eq(2)
+    end
+  end
+
+  # after(:context) do
+  #   puts 'After context'
+  # end
+
+  # after(:example) do
+  #   puts 'After example'
+  # end
+end
